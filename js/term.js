cb47c6f185300550ef1a3e530df822196f2c09ad
Meegul304 <micha3lbilstein@gmail.com>
2016-08-20 13:14:33 -0400

    Linting

--- js/term.js
+++ js/term.js
@@ -1,38 +1,21 @@
 var histNum = 0; //Total number of history elements.
 var topDir = ""; //Top of the dirStack.
-var dirStack = ['/']; //Stack of current directory chain.
+var dirStack = ["/"]; //Stack of current directory chain.
 var commandNum = 0; //Total number of commands.
 var commandStack = []; //Remembers previous commands.
 var lookingAtCommand = 0; //Command index that is currently in the input.
 var currentCommand = ""; //Saves a command that has not been entered yet when going through the history/
-var fileStructure = new Object();
-fileStructure['/'] = new Object();
+var fileStructure = Object.create(null);
+fileStructure["/"] = Object.create(null);
 
 
-document.onclick = function() {
+document.onclick = function () {
     document.getElementById("input").focus();
-}
-
-document.getElementById("input").onkeydown = function(event) {
-    var input = document.getElementById("input");
-    switch (event.keyCode) {
-        case 13:
-            enter(input);
-            break;
-        case 38:
-            upKey();
-            break;
-        case 40:
-            downKey();
-            break;
-        default:
-            break;
-    }
-}
+};
 
 /**
  * Handles what happens when the enter key is pressed.
- * Doesn't return anything.
+ * Doesn"t return anything.
  */
 
 function enter(input) {
@@ -44,7 +27,7 @@ function enter(input) {
     addHistory(result); //Adds result of command to history in the DOM, if one is given.
 }
 
-function upKey() {
+function upKey(input) {
     if (lookingAtCommand > 0) {
         if (lookingAtCommand === commandNum) {
             currentCommand = input.value;
@@ -54,12 +37,12 @@ function upKey() {
     }
 }
 
-function downKey() {
+function downKey(input) {
     if (lookingAtCommand < commandNum - 1) {
         lookingAtCommand++;
         input.value = commandStack[lookingAtCommand];
     } else if (lookingAtCommand === commandNum - 1) { //We can assume that if this is ever true, that the user has hit the up key before, meaning we have a command saved to restore.
-        lookingAtCommand++;                           //If that's not true... well, that's quite a shame.
+        lookingAtCommand++;                           //If that"s not true... well, that"s quite a shame.
         input.value = currentCommand;
     }
 }
@@ -71,18 +54,18 @@ function downKey() {
  */
 
 function parseInput(input) {
-    var inArr = input.split(' ');
+    var inArr = input.split(" ");
     switch (inArr[0]) {
-        case 'cd':
-            return cd(inArr[1]);
-        case 'pwd':
-            return oneLine(pwd());
-        case 'ls':
-            return ls(inArr[1]);
-        case 'mkdir':
-            return mkdir(inArr[1]);
-        default:
-            return oneLine('Command not found: ' + inArr[0]);
+    case "cd":
+        return cd(inArr[1]);
+    case "pwd":
+        return oneLine(pwd());
+    case "ls":
+        return ls(inArr[1]);
+    case "mkdir":
+        return mkdir(inArr[1]);
+    default:
+        return oneLine("Command not found: " + inArr[0]);
     }
 }
 
@@ -96,19 +79,20 @@ function addHistory(result) {
     var newParent = document.createElement("div");
     var newChild = document.createElement("p");
     var input = document.getElementById("input");
-    if (dirStack.slice(-1)[0] === undefined)
-        newChild.innerHTML = '/' + " " + result;
-    else
+    if (dirStack.slice(-1)[0] === undefined) {
+        newChild.innerHTML = "/" + " " + result;
+    } else {
         newChild.innerHTML = dirStack.slice(-1)[0] + " " + result;
-    if (result != input.value) {
+    }
+    if (result !== input.value) {
         clearInput();
     }
-    if (result != "nonono") {
+    if (result !== "nonono") {
         newParent.id = "his" + histNum++;
         newParent.appendChild(newChild);
         history.appendChild(newParent);
     }
-    window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight);
 }
 
 function clearInput() {
@@ -118,50 +102,51 @@ function clearInput() {
 
 function cd(input) {
     var fullDir = pwd();
-    var lookingAt = fileStructure['/'];
-    fullDir.split('/').forEach(function(element) {
-        if (lookingAt[element] != undefined)
+    var lookingAt = fileStructure["/"];
+    fullDir.split("/").forEach(function (element) {
+        if (lookingAt[element] !== undefined) {
             lookingAt = lookingAt[element];
-    }, this);
-    if (input === '..') {
-        if (dirStack.slice(-1)[0] != dirStack[0]) { //Ensures user does not navigate past root directory.
+        }
+    });
+    if (input === "..") {
+        if (dirStack.slice(-1)[0] !== dirStack[0]) { //Ensures user does not navigate past root directory.
             dirStack.pop();
             document.getElementById("headInfo").innerHTML = dirStack.slice(-1)[0];
         } else {
             clearInput();
         }
-    } else if(input === '/') {
-        dirStack = ['/'];
+    } else if (input === "/") {
+        dirStack = ["/"];
         document.getElementById("headInfo").innerHTML = dirStack.slice(-1)[0];
-    } else if (lookingAt[input] != undefined){
+    } else if (lookingAt[input] !== undefined) {
         dirStack.push(input);
         document.getElementById("headInfo").innerHTML = dirStack.slice(-1)[0];
     } else {
         oneLine("cd: No such file or directory: " + input);
     }
-    return("nonono"); //Returns this value to signify that this command has no output.
+    return "nonono"; //Returns this value to signify that this command has no output.
 }
 
 function pwd() {
-    return '/' + dirStack.slice(1).join('/');
+    return "/" + dirStack.slice(1).join("/");
 }
 
 function ls() {
     var lookingAt = retTopObj();
-    Object.keys(lookingAt).forEach(function(element) {
+    Object.keys(lookingAt).forEach(function (element) {
         oneLine(element);
-    }, this);
-    return 'nonono';
+    });
+    return "nonono";
 }
 
 function mkdir(input) {
     var lookingAt = retTopObj();
-    if (lookingAt[input] == undefined) { //Only creates new hash if key doesn't already exist.
-        lookingAt[input] = new Object();
+    if (lookingAt[input] === undefined) { //Only creates new hash if key doesn"t already exist.
+        lookingAt[input] = Object.create(null);
     } else {
-        oneLine("Cannot create directory '" + input + "': File exists");
+        oneLine("mkdir: Cannot create directory " + input + ": File exists");
     }
-    return ('nonono');
+    return ("nonono");
 }
 
 /**
@@ -170,18 +155,19 @@ function mkdir(input) {
 
 function retTopObj() {
     var fullDir = pwd();
-    var lookingAt = fileStructure['/'];
-    fullDir.split('/').forEach(function(element) {
-        if (lookingAt[element] != undefined)
+    var lookingAt = fileStructure["/"];
+    fullDir.split("/").forEach(function (element) {
+        if (lookingAt[element] !== undefined) {
             lookingAt = lookingAt[element];
-    }, this);
+        }
+    });
     return lookingAt;
 }
 
 
 /**
  * Creates a new history item that is only one line with the message passed.
- * Useful for when you don't want to include the current top directory.
+ * Useful for when you don"t want to include the current top directory.
  */
 
 function oneLine(message) {
@@ -193,6 +179,22 @@ function oneLine(message) {
     newParent.appendChild(newChild);
     history.appendChild(newParent);
     clearInput();
-    window.scrollTo(0,document.body.scrollHeight);
-    return('nonono');
+    window.scrollTo(0, document.body.scrollHeight);
+    return "nonono";
 }
+
+
+document.getElementById("input").onkeydown = function (event) {
+    var input = document.getElementById("input");
+    switch (event.keyCode) {
+    case 13:
+        enter(input);
+        break;
+    case 38:
+        upKey(input);
+        break;
+    case 40:
+        downKey(input);
+        break;
+    }
+};
