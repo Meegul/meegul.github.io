8c549e4a49f420817d1b4bdb90e2ca561642164c
Meegul304 <micha3lbilstein@gmail.com>
2016-12-27 22:12:10 -0600

    Changed day-night cycle darkness

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function updateMask() {
     const backgroundMask = document.getElementById("backgroundMask");
     const time = new Date().getHours();
-    const opacity = Math.abs(12 - time) * 0.05;
+    const opacity = Math.abs(12 - time) * 0.07;
     backgroundMask.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
     console.log(`New opacity: ${opacity}`);
 };
