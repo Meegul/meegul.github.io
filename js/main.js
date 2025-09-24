1dfbcc2883df63562804de31db72051538e9d801
Meegul <micha3lbilstein@gmail.com>
2025-09-24 17:36:58 -0500

    initial refactor before figuring out if we want to scale the box

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ window.onload = () => {
         interactive: false,
         repel: false,
         particleColor: "rgb(255,255,255)",
-        backgroundColor: "rgba(15,15,15,100)",
+        backgroundColor: "rgba(0,0,0,100)",
         connected: false,
         random: {
             number: 250,
@@ -20,19 +20,188 @@ window.onload = () => {
             maxDy: 0.3,
         },
     }, canvas).start();
-};
 
-const toggleContent = () => {
-    const contentEl = document.getElementById("content");
-    const maskEl = document.getElementById("backgroundMask");
-    const buttonEl = document.getElementById("contentToggle");
-    if (contentEl.style.display) {
-        contentEl.style.display = "";
-        maskEl.className = "background-mask";
-        buttonEl.innerHTML = "Hide everything";
-    } else {
-        contentEl.style.display = "none";
-        maskEl.className = "";
-        buttonEl.innerHTML = "Show everything";
+    const lgcanvas = document.getElementById("lgcanvas");
+    resizeLgCanvas = () => {
+        lgcanvas.width = window.innerWidth;
+        lgcanvas.height = window.innerHeight;
     }
+    resizeLgCanvas();
+    window.addEventListener("resize", resizeLgCanvas);
+    
+    const gl = lgcanvas.getContext("webgl");
+    const src_canvas = canvas;
+    
+    const vsSource = `
+        attribute vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    `;
+    
+    const fsSource = `
+        precision mediump float;
+
+        uniform vec3 iResolution;
+        uniform vec4 iCenter;
+        uniform vec2 iSize;
+        uniform sampler2D iChannel0;
+
+        void mainImage(out vec4 fragColor, in vec2 fragCoord)
+        {
+            const float NUM_ZERO = 0.0;
+            const float NUM_ONE = 1.0;
+            const float NUM_HALF = 0.5;
+            const float NUM_TWO = 2.0;
+            const float POWER_EXPONENT = 6.0;
+            const float MASK_MULTIPLIER_1 = 10000.0;
+            const float MASK_MULTIPLIER_2 = 9500.0;
+            const float MASK_MULTIPLIER_3 = 11000.0;
+            const float LENS_MULTIPLIER = 3000.0;
+            const float MASK_STRENGTH_1 = 8.0;
+            const float MASK_STRENGTH_2 = 16.0;
+            const float MASK_STRENGTH_3 = 2.0;
+            const float MASK_THRESHOLD_1 = 0.95;
+            const float MASK_THRESHOLD_2 = 0.9;
+            const float MASK_THRESHOLD_3 = 1.5;
+            const float SAMPLE_RANGE = 4.0;
+            const float SAMPLE_OFFSET = 0.5;
+            const float GRADIENT_RANGE = 0.2;
+            const float GRADIENT_OFFSET = 0.01;
+            const float GRADIENT_EXTREME = -1000.0;
+            const float LIGHTING_INTENSITY = 0.11;
+
+            vec2 uv = fragCoord / iResolution.xy;
+            vec2 center = iCenter.xy;
+            vec2 boxSize = iSize.xy;
+            vec2 m2 = (uv - center / iResolution.xy);
+
+            float roundedBox = pow(abs(m2.x * iResolution.x / iResolution.y), POWER_EXPONENT) + pow(abs(m2.y), POWER_EXPONENT);
+            float rb1 = clamp((NUM_ONE - roundedBox * MASK_MULTIPLIER_1) * MASK_STRENGTH_1, NUM_ZERO, NUM_ONE);
+            float rb2 = clamp((MASK_THRESHOLD_1 - roundedBox * MASK_MULTIPLIER_2) * MASK_STRENGTH_2, NUM_ZERO, NUM_ONE) -
+            clamp(pow(MASK_THRESHOLD_2 - roundedBox * MASK_MULTIPLIER_2, NUM_ONE) * MASK_STRENGTH_2, NUM_ZERO, NUM_ONE);
+            float rb3 = clamp((MASK_THRESHOLD_3 - roundedBox * MASK_MULTIPLIER_3) * MASK_STRENGTH_3, NUM_ZERO, NUM_ONE) -
+            clamp(pow(NUM_ONE - roundedBox * MASK_MULTIPLIER_3, NUM_ONE) * MASK_STRENGTH_3, NUM_ZERO, NUM_ONE);
+
+            fragColor = vec4(NUM_ZERO);
+            float transition = smoothstep(NUM_ZERO, NUM_ONE, rb1 + rb2);
+
+            if (transition > NUM_ZERO) {
+                vec2 lens = ((uv - NUM_HALF) * NUM_ONE * (NUM_ONE - roundedBox * LENS_MULTIPLIER) + NUM_HALF);
+                float total = NUM_ZERO;
+                for (float x = -SAMPLE_RANGE; x <= SAMPLE_RANGE; x++) {
+                    for (float y = -SAMPLE_RANGE; y <= SAMPLE_RANGE; y++) {
+                        vec2 offset = vec2(x, y) * SAMPLE_OFFSET / iResolution.xy;
+                        fragColor += texture2D(iChannel0, offset + lens);
+                        total += NUM_ONE;
+                    }
+                }
+                fragColor /= total;
+
+                float gradient = clamp((clamp(m2.y, NUM_ZERO, GRADIENT_RANGE) + GRADIENT_OFFSET) / NUM_TWO, NUM_ZERO, NUM_ONE) +
+                    clamp((clamp(-m2.y, GRADIENT_EXTREME, GRADIENT_RANGE) * rb3 + GRADIENT_OFFSET) / NUM_TWO, NUM_ZERO, NUM_ONE);
+                vec4 lighting = clamp(fragColor + vec4(rb1) * gradient + vec4(rb2) * LIGHTING_INTENSITY, NUM_ZERO, NUM_ONE);
+
+                fragColor = mix(texture2D(iChannel0, uv), lighting, transition);
+            } else {
+                fragColor = texture2D(iChannel0, uv);
+            }
+        }
+
+        void main() {
+            mainImage(gl_FragColor, gl_FragCoord.xy);
+        }
+    `;
+    
+    const createShader = (type, source) => {
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+    
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+            return null;
+        }
+    
+        return shader;
+    }
+    
+    const vs = createShader(gl.VERTEX_SHADER, vsSource);
+    const fs = createShader(gl.FRAGMENT_SHADER, fsSource);
+    const program = gl.createProgram();
+    
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error("An error occurred linking the program: " + gl.getProgramInfoLog(program));
+        return null;
+    }
+    
+    gl.useProgram(program);
+    
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]),
+        gl.STATIC_DRAW
+    )
+    
+    const position = gl.getAttribLocation(program, "position");
+    gl.enableVertexAttribArray(position);
+    gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+    
+    const uniforms = {
+        resoulution: gl.getUniformLocation(program, "iResolution"),
+        center: gl.getUniformLocation(program, "iCenter"),
+        texture: gl.getUniformLocation(program, "iChannel0"),
+        size: gl.getUniformLocation(program, "iSize"),
+    }
+
+    let textDiv = document.getElementById("textContent");
+    
+    let centerX = textDiv.offsetLeft + textDiv.offsetWidth / 2;
+    let centerY = lgcanvas.height - (textDiv.offsetTop + textDiv.offsetHeight / 2);
+    let center = [centerX, centerY];
+    console.log(center);
+
+    let sizeX = textDiv.offsetWidth;
+    let sizeY = textDiv.offsetHeight;
+    let size = [sizeX, sizeY];
+    console.log(size);
+
+
+    const render = () => {
+        gl.viewport(0, 0, lgcanvas.width, lgcanvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.uniform3f(uniforms.resoulution, lgcanvas.width, lgcanvas.height, 1.0);
+        gl.uniform4f(uniforms.center, center[0], center[1], 0, 0);
+        gl.uniform2f(uniforms.size, size[0], size[1]);
+
+        const texture = gl.createTexture();
+        const setupTexture = () => {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, src_canvas);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        }
+    
+        setupTexture();
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.uniform1i(uniforms.texture, 0);
+
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+        gl.deleteTexture(texture);
+
+        requestAnimationFrame(render);
+    }
+
+    render();
 };
